5ab35de66796a445bd637a53b73ced82c9ca103f
bastienwcs <bastien.krettly@wildcodeschool.com>
2022-03-30 12:46:20 +0200

    add env variables and cors default configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const path = require("path");
 
+const cors = require("cors");
+
 // let's create express app
 
 const app = express();
 
 // use some application-level middlewares
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
