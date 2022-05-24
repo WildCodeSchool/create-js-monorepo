9c3ff36dac5adabfefc0a88da78131e3bfca6d4a
Vincent Vaur <vaur.vincent@gmail.com>
2022-05-24 10:18:21 +0200

    Change app.js to serve the REACT app from express

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const path = require("path");
-
 const cors = require("cors");
-
-// let's create express app
+const router = require("./router");
 
 const app = express();
 
@@ -16,13 +14,19 @@ app.use(
 );
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
 
-// load router
+// Serve REACT APP
+app.use(express.static(path.join(__dirname, "..", "..", "frontend", "dist")));
 
-const router = require("./router");
-
+// API routes
 app.use(router);
+
+// Redirect all requests to the REACT app
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "..", "..", "frontend", "dist", "index.html")
+  );
+});
 
 // ready to export
 module.exports = app;
