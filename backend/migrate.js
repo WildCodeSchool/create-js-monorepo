fb7033add2dd16e8c609a7410ea31e0e7bcf6c85
romaing <rocambille@gmail.com>
2022-12-23 16:31:47 +0100

    add DB_PORT for sql connection

--- backend/migrate.js
+++ backend/migrate.js
@@ -4,10 +4,11 @@ const fs = require("fs");
 const mysql = require("mysql2/promise");
 
 const migrate = async () => {
-  const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+  const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
   const connection = await mysql.createConnection({
     host: DB_HOST,
+    port: DB_PORT,
     user: DB_USER,
     password: DB_PASSWORD,
     multipleStatements: true,
--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -2,10 +2,11 @@ const fs = require("fs");
 const mysql = require("mysql2/promise");
 const path = require("path");
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
 const pool = mysql.createPool({
   host: DB_HOST,
+  port: DB_PORT,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
