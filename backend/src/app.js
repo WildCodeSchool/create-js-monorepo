ba9e42a0b1009eb88d6fd7b7fb3e717bc52ef6b6
romaing <rocambille@gmail.com>
2023-01-17 13:20:27 +0100

    removed fallback html served by backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -53,48 +53,6 @@ if (fs.existsSync(reactIndexFile)) {
   app.get("*", (req, res) => {
     res.sendFile(reactIndexFile);
   });
-} else {
-  // serve HTML listing all the application routes as fallback
-
-  app.get("/", (req, res) => {
-    res.send(`
-      <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="utf-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1">
-          <title>Hello Wilder !</title>
-          <link rel="preconnect" href="https://fonts.googleapis.com">
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-          <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
-          <link rel="stylesheet" href="/assets/css/style.css">
-          <link rel="icon" href="/assets/images/favicon.png">
-        </head>
-        <body>
-          <h1>Hello Wilder !</h1>
-          <p>Here are all your application routes:</p>
-          <table>
-            ${router.stack
-              .map(
-                ({ route }) => `
-                  <tr>
-                    <td style="font-weight: bold; padding-right: 4rem">
-                      ${Object.keys(route.methods)
-                        .map((method) => method.toUpperCase())
-                        .join(", ")}
-                    </td>
-                    <td>
-                      ${route.path}
-                    </td>
-                  </tr>
-                `
-              )
-              .join("")}
-          </table>
-        </body>
-      </html>
-    `);
-  });
 }
 
 // ready to export
