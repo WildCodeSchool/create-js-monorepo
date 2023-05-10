4a5ac9ba349ab1fc061e10fbec1e707445a08e73
tony3717 <gentytony@gmail.com>
2023-05-10 23:30:41 +0200

    envoi du reste

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
+import Page2 from "./pages/Page2";
+import NavBar from "./pages/NavBar";
+import Footer from "./pages/Footer";
+import Page1 from "./pages/Page1";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/page1" element={<Page1 />} />
+          <Route path="/page2" element={<Page2 />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
