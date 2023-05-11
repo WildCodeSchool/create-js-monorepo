22e17ca50d0c1a690966bd3fe837023d2816f956
Jumzer <bluevictor@hotmail.fr>
2023-05-11 13:18:13 +0200

    fetching the data

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Home() {
-  return <div>Home</div>;
+  const [Events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.seatgeek.com/2/events?client_id=MzM1ODcyMTh8MTY4Mzc5NzY5MC41MDE4NQ`
+    )
+      .then((data) => data.json())
+      .then((events) => setEvents(events))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return <div>{Events}</div>;
 }
 
 export default Home;
