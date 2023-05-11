d372cdd6dae317c44d65c8eb26c433875ac7ea86
Jumzer <bluevictor@hotmail.fr>
2023-05-11 13:19:34 +0200

    fetching data

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     fetch(
       `https://api.seatgeek.com/2/events?client_id=MzM1ODcyMTh8MTY4Mzc5NzY5MC41MDE4NQ`
     )
-      .then((data) => data.json())
+      .then((res) => res.json())
       .then((events) => setEvents(events))
       .catch((err) => console.error(err));
   }, []);
