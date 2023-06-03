1376f436b361e89efe52ee31654a4164ceeb0bfd
romaing <rocambille@gmail.com>
2023-06-03 15:17:28 +0200

    removed useless dependency in frontend + installed react-router-dom and refactored main/App

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
