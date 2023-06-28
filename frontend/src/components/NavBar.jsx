933cd835fe8223e000a8d8e69d03ba3275ab7d00
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-06-28 15:40:07 +0200

    navbar desktop styled

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,20 +5,31 @@ import interrogation from "../assets/interrogation.png";
 
 function NavBar() {
   return (
-    <div className="nav flex items-center flex-col justify-between fixed top-0 left-0 w-1/5 h-screen bg-[#002743]">
-      <img src={Logo} alt="logo" />
-      <div className="navbar-buttons">
-        <button type="button" className="flex items-center justify-between">
-          <img src={addSymbol} alt="symbole plus" />
-          <p>Estimer un smartphone</p>
-        </button>
-        <button type="button">
-          <img src={addSymbol} alt="symbole plus" />
-          <p>Ajouter des références</p>
-        </button>
+    <div className="nav flex items-center flex-col justify-between fixed top-0 left-0 w-1/5 h-screen text-white bg-[#002743]">
+      <div className="flex flex-col items-center gap-20 w-full">
+        <img src={Logo} alt="logo" className="w-[30%] mt-[56px]" />
+        <div className="navbar-buttons w-[100%] px-7 flex flex-col gap-5 text-lg">
+          <button
+            type="button"
+            className="flex items-center align-baseline gap-2 w-[100%] "
+          >
+            <img src={addSymbol} alt="symbole plus" className="w-[14px]" />
+            <p>Estimer un smartphone</p>
+          </button>
+          <button
+            type="button"
+            className="flex items-center align-baseline gap-2 w-[100%] "
+          >
+            <img src={addSymbol} alt="symbole plus" className="w-[14px]" />
+            <p>Ajouter des références</p>
+          </button>
+        </div>
       </div>
-      <div>
-        <button type="button">
+      <div className="w-[100%] flex flex-col items-center justify-between">
+        <button
+          type="button"
+          className="flex items-center justify-center gap-3 w-[100%] mb-[56px]"
+        >
           <img src={interrogation} alt="symbole plus" />
           <p>FAQ</p>
         </button>
