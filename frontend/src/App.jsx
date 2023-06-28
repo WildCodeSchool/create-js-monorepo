e8777ae2de2592206b4241ff5f31da1f1d709071
JIMI_BES <jimi.bes@gmail.com>
2023-06-28 14:57:18 +0200

    useless functions and style deleted, reset css initialized and HomePage created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <p>coucou</p>
     </div>
   );
 }
---
+++ frontend/src/components/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+
+export default HomePage;
