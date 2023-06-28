83ec7993f0fa9a7b6cb064009e71f63f16719166
JIMI_BES <jimi.bes@gmail.com>
2023-06-28 17:16:27 +0200

    3d phone animation added

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "../styles/HomePage.scss";
+import Lottie from "react-lottie-player";
+import animationData from "../assets/25889-phone-flow-3d.json";
 
 function HomePage() {
   return (
@@ -7,6 +9,12 @@ function HomePage() {
       <h1>Commencer l'estimation du prix des téléphones</h1>
       <button type="button">Commencer l'estimation</button>
       <button type="button">Ajouter des références</button>
+      <Lottie
+        animationData={animationData}
+        play
+        loop
+        style={{ width: 500, height: 500 }}
+      />
     </div>
   );
 }
