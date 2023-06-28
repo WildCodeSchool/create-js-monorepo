5c9555c8a41391a1a703e1d79f74c88430ebffb4
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-06-28 15:22:31 +0200

    Navbar 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import NavBar from "./components/NavBar";
-import Home from "./pages/Home";
 import "./reset.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
       <NavBar />
     </div>
   );
