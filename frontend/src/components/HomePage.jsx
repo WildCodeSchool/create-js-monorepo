c43d1a9c0ff6d7d01b5ae49a397dfe53ac8d2409
JIMI_BES <jimi.bes@gmail.com>
2023-06-28 17:10:52 +0200

    Homepage created and style added

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+import "../styles/HomePage.scss";
+
 function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="home-page-container">
+      <h1>Commencer l'estimation du prix des téléphones</h1>
+      <button type="button">Commencer l'estimation</button>
+      <button type="button">Ajouter des références</button>
     </div>
   );
 }
