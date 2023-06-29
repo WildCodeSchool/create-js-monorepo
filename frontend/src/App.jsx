a88405883f7f3bb63cf36cb10dbc316ee8946475
Yann Richard <yann.richard9@gmail.com>
2023-06-29 11:51:38 +0200

    added brand, storage, and memory pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Home from "./pages/user/Home";
+import Brand from "./pages/user/Brand";
+import Storage from "./pages/user/Storage";
+import Memory from "./pages/user/Memory";
 import "react-toastify/dist/ReactToastify.css";
 
 import "./App.css";
@@ -10,6 +13,9 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/brand" element={<Brand />} />
+        <Route path="/storage" element={<Storage />} />
+        <Route path="/memory" element={<Memory />} />
       </Routes>
       <ToastContainer />
     </div>
