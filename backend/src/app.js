deac85d9428be5edb61015ff46e5c6efca6a51d1
romaing <rocambille@gmail.com>
2023-10-09 16:19:10 +0200

    added req info in backend error middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -100,6 +100,7 @@ app.use("/api", router);
 const logErrors = (err, req, res, next) => {
   // Log the error to the console for debugging purposes
   console.error(err);
+  console.error("on req:", req.method, req.path);
 
   // Pass the error to the next middleware in the stack
   next(err);
