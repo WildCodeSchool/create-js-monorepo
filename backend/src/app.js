9a3a803f5bba41440457f855d6d8d35e72101d12
romaing <rocambille@gmail.com>
2023-10-10 12:01:41 +0200

    commented front service in backend + moved it before error middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -92,6 +92,37 @@ app.use("/api", router);
 
 /* ************************************************************************* */
 
+// Production-ready setup: What is it for, and when should I enable it?
+
+// The code includes commented sections to set up a production environment where the frontend and backend are served from the same server.
+
+// What it's for:
+// - Serving frontend static files from the backend, which is useful when building a single-page application with React, Angular, etc.
+// - Redirecting unhandled requests (e.g., all requests not matching a defined API route) to the frontend's index.html. This allows the frontend to handle client-side routing.
+
+// When to enable it:
+// It depends on your project and its structure. If you are developing a single-page application, you'll enable these sections when you are ready to deploy your project to production.
+
+// To enable production configuration:
+// 1. Uncomment the lines related to serving static files and redirecting unhandled requests.
+// 2. Ensure that the `reactBuildPath` points to the correct directory where your frontend's build artifacts are located.
+
+/*
+const reactBuildPath = `${__dirname}/../../frontend/dist`;
+
+// Serve react resources
+
+app.use(express.static(reactBuildPath));
+
+// Redirect unhandled requests to the react index file
+
+app.get("*", (req, res) => {
+  res.sendFile(`${reactBuildPath}/index.html`);
+});
+*/
+
+/* ************************************************************************* */
+
 // Middleware for Error Logging (Uncomment to enable)
 // Important: Error-handling middleware should be defined last, after other app.use() and routes calls.
 
@@ -109,37 +140,6 @@ const logErrors = (err, req, res, next) => {
 // Mount the logErrors middleware globally
 app.use(logErrors);
 */
-
-/* ************************************************************************* */
-
-// Production-ready setup: What is it for, and when should I enable it?
-
-// The code includes commented sections to set up a production environment where the frontend and backend are served from the same server.
-
-// What it's for:
-// - Serving frontend static files from the backend, which is useful when building a single-page application with React, Angular, etc.
-// - Redirecting unhandled requests (e.g., all requests not matching a defined API route) to the frontend's index.html. This allows the frontend to handle client-side routing.
-
-// When to enable it:
-// It depends on your project and its structure. If you are developing a single-page application, you'll enable these sections when you are ready to deploy your project to production.
-
-// To enable production configuration:
-// 1. Uncomment the lines related to serving static files and redirecting unhandled requests.
-// 2. Ensure that the `reactBuildPath` points to the correct directory where your frontend's build artifacts are located.
-
-
-const reactBuildPath = `${__dirname}/../../frontend/dist`;
-
-// Serve react resources
-
-app.use(express.static(reactBuildPath));
-
-// Redirect unhandled requests to the react index file
-
-app.get("*", (req, res) => {
-  res.sendFile(`${reactBuildPath}/index.html`);
-});
-
 
 /* ************************************************************************* */
 
