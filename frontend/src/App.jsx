30a2fc1660d15e772a3e9a3a01e53d96f93fb709
ArthurHtbk <arthur.heurtebise@gmail.com>
2023-10-31 11:22:36 +0100

    fix: adapt to later version of the template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>Coucou Vite + React!</p>
 
         <Counter />
 
