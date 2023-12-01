131fef30fe7fac7dca34e514ec95137b626064c8
Zonghero Jean Marc <jm.zonghero@gmail.com>
2023-12-01 12:01:44 +0100

    création du chemin SQL

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,21 @@
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
-);
+-- SQLBook: Code
+
+
+CREATE TABLE
+    infoclub (
+        id int primary key auto_increment not null,
+        label VARCHAR(255) NOT NULL,
+        number_title_off_champion VARCHAR(255) NOT NULL,
+        number_title_off_national_cup VARCHAR(255) NOT NULL,
+        number_title_off_europ_cup VARCHAR(255) not NULL
+    );
+
+    CREATE TABLE
+    team (
+        id int unsigned primary key auto_increment not null,
+        club_id INTEGER NOT NULL,
+        city VARCHAR(255) NOT NULL,
+        stade VARCHAR(255) NOT NULL,
+        create_date year not null,
+        FOREIGN KEY(club_id) REFERENCES infoclub(id)
+    );
