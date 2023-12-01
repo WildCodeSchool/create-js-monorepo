137965cf0128b321ae3aa345f18edc98eb5a0123
Zonghero Jean Marc <jm.zonghero@gmail.com>
2023-12-01 14:39:32 +0100

    création du chemin SQL

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -10,6 +10,13 @@ CREATE TABLE
         number_title_off_europ_cup VARCHAR(255) not NULL
     );
 
+     INSERT INTO infoclub(number_title_off_champion, number_title_off_national_cup, number_title_off_europ_cup) VALUE ('11', '14', '1');
+     INSERT INTO infoclub(number_title_off_champion, number_title_off_national_cup, number_title_off_europ_cup) VALUE ('9', '10', '1');
+     INSERT INTO infoclub(number_title_off_champion, number_title_off_national_cup, number_title_off_europ_cup) VALUE ('4', '3', '0');
+     INSERT INTO infoclub(number_title_off_champion, number_title_off_national_cup, number_title_off_europ_cup) VALUE ('11', '14', '0');
+     INSERT INTO infoclub(number_title_off_champion, number_title_off_national_cup, number_title_off_europ_cup) VALUE ('11', '14', '0');
+     INSERT INTO infoclub(number_title_off_champion, number_title_off_national_cup, number_title_off_europ_cup) VALUE ('11', '14', '0')
+
     CREATE TABLE
     team (
         id int unsigned primary key auto_increment not null,
@@ -19,3 +26,14 @@ CREATE TABLE
         create_date year not null,
         FOREIGN KEY(club_id) REFERENCES infoclub(id)
     );
+
+     INSERT INTO team(club_id, city, stade, create_date) VALUES ('PSG', 'Paris', 'Parc des Prince', '1970');
+
+     INSERT INTO team(club_id, city, stade, create_date) VALUES ('OM', 'Marseille', 'Orange Vélodrome', '1904');
+    NSERT INTO team(club_id, city, stade, create_date) VALUES ('OGCN', 'Nice', 'L'Allianz Riviera', '1908');
+    NSERT INTO team(club_id, city, stade, create_date) VALUES ('SR', 'Rennes', 'Roazon Park', '1970');
+
+    NSERT INTO team(club_id, city, stade, create_date) VALUES ('RCL', 'Lens', 'Stade Bollaert', '1970');
+    NSERT INTO team(club_id, city, stade, create_date) VALUES ('AJA', 'Auxerre', 'Stade Abbé Deschamp', '1970');
+
+
