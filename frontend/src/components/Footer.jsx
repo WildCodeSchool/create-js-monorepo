df4707f73170a0263c5653a7b391d219849b3d9c
Thibaud Dupuis <124290694+ThibaudDps@users.noreply.github.com>
2023-12-04 11:53:56 +0100

    about + footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,11 @@ import React from "react";
 import "../style/Footer.css";
 
 function Footer() {
-  return <div className="footer">Caught with ❤️ by Thibaud</div>;
+  return (
+    <div className="footer">
+      <p className="p-footer">Caught with ❤️ by Thibaud</p>
+    </div>
+  );
 }
 
 export default Footer;
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+import Seye from "../assets/seye.gif";
+
+import "../style/About.css";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className="about-body">
+        <NavLink to="https://github.com/ThibaudDps">
+          {" "}
+          <img className="seye" src={Seye} alt="seye" />
+        </NavLink>
+
+        <h1 className="h1-about">Hey, I'm Thibaud !</h1>
+        <p className="p-about">
+          I'm a Pkmn trainer since 95' and a Full Stack Developer since 23'
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
