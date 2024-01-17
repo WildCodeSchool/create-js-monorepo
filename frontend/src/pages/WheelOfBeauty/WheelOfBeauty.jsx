f20a0ce31742d9546488afd1b5ed346f21c2ce56
JulienOza <julien.ozarowski@gmail.com>
2024-01-17 23:20:16 +0100

    integrating wheel in wheel page

--- frontend/src/pages/WheelOfBeauty/WheelOfBeauty.jsx
+++ frontend/src/pages/WheelOfBeauty/WheelOfBeauty.jsx
@@ -1,7 +1,13 @@
+import Routourne from "../../components/Routourne/Routourne";
 import "./WheelOfbeauty.scss";
 
 function WheelOfbeauty() {
-  return <h1 className="wheel">hi hi wheel of beauty page</h1>;
+  return (
+    <>
+      <h1 className="wheel">hi hi wheel of beauty page</h1>
+      <Routourne />
+    </>
+  );
 }
 
 export default WheelOfbeauty;
