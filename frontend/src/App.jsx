df5afe8bf9af9bc05b308dae9041635a08bcbf86
Leslie2186 <leslie.moraud@gmail.com>
2024-01-18 11:25:47 +0100

    Initialisation page Histoire

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 
 import "./App.css";
--- frontend/src/pages/Histoire.jsx
+++ frontend/src/pages/Histoire.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import backgroundHistoire from "../assets/concept.jpg";
 
 function Histoire() {
   return (
     <div className="Histoire">
-      <h3>Histoire</h3>
+      <h3>Les origines de L'Oréal</h3>
+      <div>
+        <img src={backgroundHistoire} alt="Décore de fond Histoire" />
+      </div>
+      <div>
+        <p>
+          L’Oréal est un groupe industriel français de produits cosmétiques. La
+          société, créée par Eugène Schueller le 30 juillet 1909, est de nos
+          jours devenue le groupe international numéro un mondial de l'industrie
+          cosmétique
+        </p>
+      </div>
     </div>
   );
 }
