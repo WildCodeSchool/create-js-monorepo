ce472a995533162c15f030f6c008dc6f965ede67
Alexis-NM <alexis.na.me@gmail.com>
2024-01-18 09:02:07 +0100

    modal_styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,27 +1,29 @@
 import React, { useState } from "react";
-import LoginModal from "../components/LoginModal";
+import LoginModal from "../components/LoginModal"; // Add missing import statement
 import Logo from "../assets/loreal_logo.svg";
 import "./Home.scss";
 import "../commons.scss";
 
 function Home() {
   const [showAvatar, setShowAvatar] = useState(false);
+  const [modalKey, setModalKey] = useState(0);
 
   const openLoginModal = () => {
     setShowAvatar(true);
+    setModalKey((prevKey) => prevKey + 1);
   };
 
   return (
     <div className="home-container">
       <img src={Logo} alt="Logo Loreal" className="logo-loreal" />
-      <h2 className="main-title">L'OR&#xC9;AL AVATAR</h2>
+      <h2 className="main-title">L'ORÉAL AVATAR</h2>
       <input
         type="button"
         value="Créer un compte"
         onClick={openLoginModal}
         className="button-center"
       />
-      {showAvatar && <LoginModal />}
+      {showAvatar && <LoginModal key={modalKey} />}
     </div>
   );
 }
