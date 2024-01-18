dc1d619bedfadc826b912d55f7633342e93764b0
vinz-DY <vincentducamp259@hotmail.com>
2024-01-18 11:38:27 +0100

    css ok id

--- frontend/src/components/DisplayCardID/DisplayCardId.jsx
+++ frontend/src/components/DisplayCardID/DisplayCardId.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import CardItemId from "../CardItemId/CardItemId";
+import "./displayCardId.css";
 
 function DisplayCardId({ basePath }) {
   const items = useLoaderData();
--- frontend/src/pages/CardsId/CardsId.jsx
+++ frontend/src/pages/CardsId/CardsId.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import DisplayCardId from "../../components/DisplayCardID/DisplayCardId";
+import "../CardsAll/CardsAll.css";
 
 function CardsId() {
   return (
-    <div>
+    <div className="ContainerCardsAll">
       <DisplayCardId basePath="/candidats/:id" />
     </div>
   );
