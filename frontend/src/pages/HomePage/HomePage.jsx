48d356b410c123e9a244b99388c4b8af63cc7bd0
FREDERIC TICON <fred.ticon@gmail.com>
2024-01-18 10:46:54 +0100

    Update HomePage.jsx

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -2,9 +2,17 @@ import "./HomePage.scss";
 
 function HomePage() {
   return (
-    <button className="HomePage__button" type="button">
-      Login
-    </button>
+    <>
+      <button className="HomePage__button" type="button">
+        Mon compte
+      </button>
+      <img
+        className="card1"
+        name="card1"
+        src="src/assets/card1.png"
+        alt="logo"
+      />
+    </>
   );
 }
 export default HomePage;
