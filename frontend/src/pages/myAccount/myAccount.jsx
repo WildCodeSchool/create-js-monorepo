bc7479064e10755e9ff71e131d6cbe2757eadb81
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-01-18 15:37:09 +0100

    style of account page

--- frontend/src/pages/myAccount/myAccount.jsx
+++ frontend/src/pages/myAccount/myAccount.jsx
@@ -1,4 +1,5 @@
 import "./myAccount.scss";
+import { Link } from "react-router-dom";
 
 function MyAccount() {
   return (
@@ -6,29 +7,50 @@ function MyAccount() {
       <header className="myAccount__header">
         <img
           className="myAccount__header__img"
-          src="src/assets/images/barnavLoreal.png"
+          src="src/assets/images/banner.png"
           alt="our navbar"
         />
       </header>
-      <aside className="myAccount__sideBar">
-        <ul className="myAccount__sideBar__list">
-          <li className="myAccount__sideBar__list__element">Mon compte</li>
-          <li className="myAccount__sideBar__list__element">
-            Historique de commande
-          </li>
-          <li className="myAccount__sideBar__list__element">
-            Carnet d'adresses
-          </li>
-          <li className="myAccount__sideBar__list__element">
-            Liste de favoris
-          </li>
-          <li className="myAccount__sideBar__list__element">Ma box</li>
-          <li className="myAccount__sideBar__list__element disconnect">
-            Déconnection
-          </li>
-        </ul>
-      </aside>
-      <h1 className="myAccount__title">Hello account page</h1>
+      <div className="myAccount__mainContainer">
+        <aside className="myAccount__sideBar">
+          <ul className="myAccount__sideBar__list">
+            <li className="myAccount__sideBar__list__element">Mon compte</li>
+            <li className="myAccount__sideBar__list__element">
+              Historique de commande
+            </li>
+            <li className="myAccount__sideBar__list__element">
+              Carnet d'adresses
+            </li>
+            <li className="myAccount__sideBar__list__element">
+              Liste de favoris
+            </li>
+            <Link to="/wheel" className="myAccount__sideBar__list__element">
+              Ma box
+            </Link>
+            <li className="myAccount__sideBar__list__element disconnect">
+              Déconnection
+            </li>
+          </ul>
+        </aside>
+        <main className="myAccount__main">
+          <h1 className="myAccount__title">Option de compte & inscription</h1>
+          <article className="myAccount__main__article">
+            <h2 className="myAccount">MON COMPTE</h2>
+            <div className="button__countainer">
+              <button className="myAccount__button" type="submit">
+                bt1
+              </button>
+              <button className="myAccount__button" type="submit">
+                bt2
+              </button>
+            </div>
+            <h4>nom</h4>
+            <p>John Couscous</p>
+            <h4>Adresse mail</h4>
+            <p>JohnCoucous@parfum.com</p>
+          </article>
+        </main>
+      </div>
     </section>
   );
 }
