0e9d2fc86a15492b22613692acbca53051b98829
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-01-18 10:16:17 +0100

    ajout du footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
       <Outlet />
     </div>
   );
