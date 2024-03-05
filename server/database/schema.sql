453d404161ec4ea3db15c35063c4963314795251
romaing <rocambille@gmail.com>
2024-03-05 10:26:49 +0100

    restored migrate process (may be an other PR) + removed deployment relative scripts in root package

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,10 +1,10 @@
-create table if not exists user (
+create table user (
   id int unsigned primary key auto_increment not null,
   email varchar(255) not null unique,
   password varchar(255) not null
 );
 
-create table if not exists item (
+create table item (
   id int unsigned primary key auto_increment not null,
   title varchar(255) not null,
   user_id int unsigned not null,
