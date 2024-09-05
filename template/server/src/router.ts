c4e8a10f398d09716c4a9b64015b975da3cbfdbc
IDRISSI O. Ayoub <idrissio.ayoub@gmail.com>
2024-09-05 11:05:34 +0200

    update: missing types

--- template/server/src/router.ts
+++ template/server/src/router.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-const router = express.Router();
+const router: express.Router = express.Router();
 
 /* ************************************************************************* */
 // Define Your API Routes Here
