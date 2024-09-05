33d14b395cd443efe22b414a3352f456ae7660a4
IDRISSI O. Ayoub <idrissio.ayoub@gmail.com>
2024-09-05 11:08:51 +0200

    update: handling promise returns to fix warnings

--- template/bin/clean.js
+++ template/bin/clean.js
@@ -6,9 +6,9 @@ for (const nodeModules of [
   path.join(__dirname, "..", "client", "node_modules"),
   path.join(__dirname, "..", "server", "node_modules"),
 ]) {
-  fs.rm(nodeModules, { recursive: true, force: true });
+  fs.rm(nodeModules, { recursive: true, force: true }).then(() => console.info("node_modules cleaned"));
 }
 
 const packageLock = path.join(__dirname, "..", "package-lock.json");
 
-fs.rm(packageLock, { force: true });
+fs.rm(packageLock, { force: true }).then(() => console.info("package-lock.json cleaned"));
