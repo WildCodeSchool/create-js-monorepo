aa83bc31c4e4fe5c486fb18ea834a68caa8d90fb
romaing <rocambille@gmail.com>
2024-09-24 14:17:31 +0200

    added middleware example in template README

---
+++ template/server/src/types/express/index.d.ts
@@ -0,0 +1,14 @@
+// to make the file a module and avoid the TypeScript error
+export type {};
+
+declare global {
+  namespace Express {
+    export interface Request {
+      /* ************************************************************************* */
+      // Add your custom properties here, for example:
+      //
+      // user?: { ... }
+      /* ************************************************************************* */
+    }
+  }
+}
