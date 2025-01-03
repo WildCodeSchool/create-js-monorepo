408932fe501d683730fbb534c0c53c52215b59fd
Brian-boudrioux <135231975+Brian-boudrioux@users.noreply.github.com>
2025-01-03 11:33:30 +0100

    Update seed fixture import

--- template/server/bin/seed.ts
+++ template/server/bin/seed.ts
@@ -22,8 +22,8 @@ const seed = async () => {
 
     for (const filePath of filePaths) {
       const { default: SeederClass } = await import(
-        path.join(fixturesPath, filePath)
-      );
+				`file://${path.join(fixturesPath, filePath)}`
+			);
 
       const seeder = new SeederClass() as AbstractSeeder;
 
